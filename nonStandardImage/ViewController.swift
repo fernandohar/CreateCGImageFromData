f7ffc6547025225accdc2a805a837f15fc975333
Fernando <hcm415@ha.org.hk>
2021-01-06 11:59:52 +0800

    Initial Commit

---
+++ nonStandardImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nonStandardImage
+//
+//  Created by Fernando on 6/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
